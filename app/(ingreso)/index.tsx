779d0702344b2555b3a25c7cb9237a23f1982246
jeniferjaramillolopez04 <jeniferjaramillolopez04@gmail.com>
2025-04-25 23:45:04 +0000

    monte el ingreso

---
+++ app/(ingreso)/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+export default function HomeScreen() {
+  return (
+     <View >
+   
+      <Text >TrueQ U</Text>
+      <SafeAreaView>
+        <View>
+           
+        </View>
+        
+     </SafeAreaView>
+    </View>
+  );
+}
+
+
