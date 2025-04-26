0a669a0c4ce80d677c021910c72b6a65f217dbab
jeniferjaramillolopez04 <jeniferjaramillolopez04@gmail.com>
2025-04-26 00:00:17 +0000

    Formulario

--- app/(ingreso)/index.tsx
+++ app/(ingreso)/index.tsx
@@ -1,21 +1,23 @@
+import Formulario from '@/components/Formulario';
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
+
 export default function HomeScreen() {
-  return (
-     <View >
+ 
+    
+        return (
+    <View >
    
       <Text >TrueQ U</Text>
       <SafeAreaView>
         <View>
-           
+        <Formulario></Formulario> 
         </View>
         
      </SafeAreaView>
     </View>
   );
 }
-
-
